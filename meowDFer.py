a6036e18ed94fde3759acb55c4f5119b87c6a756
vincent <vinayakgeorgeciobanu@gmail.com>
2026-01-30 11:48:01 +0100

    feat: main program structure

---
+++ meowDFer.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import re 
+
+
+logo = r"""
+      |\      _,,,---,,_
+ZZZzz /,`.-'`'    -.  ;-;;,_
+     |,4-  ) )-,_. ,\ (  `'-'
+    '---''(_/--'  `-'\_)  meowDFer
+"""
+
+
+def extract_zips(src, dest):
+    return
+
+def extract_chapter_number(file_name):
+    return
+
+def file_to_pdf(file_name):
+    return
+
+def convert_all_pdf(src, dest, name):
+    return
+
+def combine_volume(src, dest, name):
+    return
+
+
+def main():
+    print(logo)
+
+    name = input("Enter name: ")
+
+    print(name)
+
+
+if __name__ == "__main__":
+    main()
