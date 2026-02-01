05a20f84aa2b2342779646e9a06669ec9fafa80d
vincent <vinayakgeorgeciobanu@gmail.com>
2026-02-01 17:38:37 +0100

    feat: add command for merge

---
+++ src/commands/merge.py
@@ -0,0 +1,25 @@
+
+from utils.utils_pdfs import merge_to_volumes
+
+def run(args):
+    src = args.src
+    dest = args.dest
+    name = args.name
+    merge_to_volumes(src, dest, name)
+
+def register_command(parser):
+    parser.add_argument(
+        "--src",
+        type=str,
+        help="Source folder where PDFs are located"
+    )
+    parser.add_argument(
+        "--dest",
+        type=str,
+        help="Name of destination folder where volumes are to be created"
+    )
+    parser.add_argument(
+        "--name",
+        type=str,
+        help="Name of the book"
+    )
--- src/meowDFer.py
+++ src/meowDFer.py
@@ -3,8 +3,7 @@ import sys
 import re
 import argparse
 
-from commands import extract, convert
-
+from commands import extract, convert, merge
 
 logo = r"""
       |\      _,,,---,,_
@@ -18,21 +17,27 @@ def main():
 
     parser = argparse.ArgumentParser(
         prog="meowDFer",
-        description="Extract zips, converts image folders into PDFs, and combines PDFs into volume. (for help use extract -h)"
+        description="Extract zips, converts image folders into PDFs, and combines PDFs into volume."
     )
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     # extract
     extract_parser = subparsers.add_parser(
-        "extract", help="Extract one or more zip files from a folder"
+        "extract", help="Extract one or more zip files from a folder. (help: extract -h)"
     )
     extract.register_command(extract_parser)
 
     # convert
     convert_parser = subparsers.add_parser(
-        "convert", help="Converts one or more folders with images into PDFs. (for help use convert -h)"
+        "convert", help="Convert one or more folders with images into PDFs. (help: convert -h)"
     )
     convert.register_command(convert_parser)
+
+    # merge
+    merge_parser = subparsers.add_parser(
+        "merge", help="Merge PDFs into volumes based on input. (help: merge -h)"
+    )
+    merge.register_command(merge_parser)
 
     args = parser.parse_args()
 
@@ -44,10 +49,16 @@ def main():
 
     elif args.command == "convert":
         if not args.src or not args.dest:
-            convert_parser.error("The --src and --dest flags are required when using -c/--convert")
+            convert_parser.error("The --src, --dest, and --name flags are required when using -c/--convert")
         print("Running convert...")
         convert.run(args)
     
+    elif args.command == "merge":
+        if not args.src or not args.dest:
+            merge_parser.error("The --src, and--dest flags are required when using -m/--merge")
+        print("Running merge...")
+        merge.run(args)
+
     else:
         parser.print_help()
 
