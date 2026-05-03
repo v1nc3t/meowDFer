6f64e77784af48dade41690984daefec449d47b2
vincent <vinayakgeorgeciobanu@gmail.com>
2026-05-03 12:39:52 +0200

    fix: extract to be transactional

--- src/commands/extract/extract_zips.py
+++ src/commands/extract/extract_zips.py
@@ -3,15 +3,15 @@ import shutil
 import tempfile
 
 from ...utils.file_utils import get_zip_files
-from zipfile import ZipFile, BadZipFile
+from zipfile import ZipFile
+
 
 def run(src_path, dest_path, to_skip, console):
     with tempfile.TemporaryDirectory() as temp_dir:
-        extract(src_path, temp_dir, to_skip, console)
+        if not extract(src_path, temp_dir, to_skip, console):
+            return False
 
-        # move from temp to dest
         os.makedirs(dest_path, exist_ok=True)
-
         for item in os.listdir(temp_dir):
             s = os.path.join(temp_dir, item)
             d = os.path.join(dest_path, item)
@@ -20,25 +20,29 @@ def run(src_path, dest_path, to_skip, console):
                 shutil.rmtree(d)
             shutil.move(s, d)
 
+    return True
+
+
 def extract(src_path, dest_path, to_skip, console):
     try:
         zip_files = get_zip_files(src_path)
     except (FileNotFoundError, ValueError) as e:
         console.print(f"[bold red]Error:[/bold red] {e}")
-        return
+        return False
 
     os.makedirs(dest_path, exist_ok=True)
-    # extraction 
     for file_name in zip_files:
         zip_path = os.path.join(src_path, file_name)
         try:
-            with ZipFile(zip_path, 'r') as zip_ref:
+            with ZipFile(zip_path, "r") as zip_ref:
                 zip_ref.extractall(dest_path)
             console.print(f"[blue]Staged:[/blue] {file_name}")
-        except Exception as e:
+        except Exception:
             if to_skip:
                 console.print(f"[bold yellow]Skipped: {file_name}[/bold yellow]")
                 continue
-        
+
             console.print(f"[bold red]Error: {file_name}.[/bold red]")
-            return
+            return False
+
+    return True
