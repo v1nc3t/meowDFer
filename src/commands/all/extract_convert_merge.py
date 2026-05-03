9fa92d4f8a946fffa3b9052508874066e89daed3
vincent <vinayakgeorgeciobanu@gmail.com>
2026-05-03 12:44:01 +0200

    fix: pipelines transactional

--- src/commands/all/extract_convert_merge.py
+++ src/commands/all/extract_convert_merge.py
@@ -1,21 +1,17 @@
 import os
 import tempfile
 
-from rich.console import Console
 from ..extract import extract_zips
 from ..convert import convert_pdf
-from ..merge import merge_pdf   
+from ..merge import merge_pdf
 
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
-console = Console()
 
 def run(src, dest, vols, name, console):
     with tempfile.TemporaryDirectory() as temp_extracted:
-        extract_zips.run(src, temp_extracted, to_skip=True, console=console)
+        if not extract_zips.run(src, temp_extracted, to_skip=True, console=console):
+            return False
 
         with tempfile.TemporaryDirectory() as temp_converted:
-            convert_pdf.run(temp_extracted, temp_converted, name, console)
-
-            merge_pdf.run(temp_converted, dest, vols, name, console)
-
-    
+            if not convert_pdf.run(temp_extracted, temp_converted, name, console):
+                return False
+            return merge_pdf.run(temp_converted, dest, vols, name, console)
