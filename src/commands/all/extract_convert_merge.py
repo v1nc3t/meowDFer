0b3b64a9fceb60c008f5068d9ce263ea6295749d
vincent <vinayakgeorgeciobanu@gmail.com>
2026-05-19 21:35:15 +0200

    fix: pipelines add folder type parameter

--- src/commands/all/extract_convert_merge.py
+++ src/commands/all/extract_convert_merge.py
@@ -6,12 +6,12 @@ from ..extract import extract_zips
 from ..convert import convert_pdf
 from ..merge import merge_pdf
 
-def run(src: str, dest: str, vols: str, name: str, to_skip: bool = False, console: Any = None) -> bool:
+def run(src: str, dest: str, vols: str, name: str, folder_type: str, to_skip: bool = False, console: Any = None) -> bool:
     with tempfile.TemporaryDirectory() as temp_extracted:
         if not extract_zips.run(src, temp_extracted, to_skip, console=console):
             return False
 
         with tempfile.TemporaryDirectory() as temp_converted:
-            if not convert_pdf.run(temp_extracted, temp_converted, name, to_skip, console):
+            if not convert_pdf.run(temp_extracted, temp_converted, name, folder_type, to_skip, console):
                 return False
             return merge_pdf.run(temp_converted, dest, vols, name, to_skip, console)
